a3bdd7b32e240db84ee5ab5417f1209cef20311d
DrBibop <olipoulihot@hotmail.com>
2021-03-16 13:43:36 -0400

    Automatically enables VR upon launch + in-game HUD fix

--- RoR2VRMod/Main.cs
+++ RoR2VRMod/Main.cs
@@ -1,10 +1,13 @@
 ﻿using BepInEx;
 using HarmonyLib;
+using MonoMod.Cil;
 using R2API.Utils;
 using RoR2;
 using System;
+using System.Collections;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Security;
 using System.Security.Permissions;
 using UnityEngine;
@@ -16,15 +19,42 @@ namespace DrBibop
 {
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync)]
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.DrBibop.VRFixes", "VR Fixes", "1.0.0")]
-    public class VRFixes : BaseUnityPlugin
+    [BepInPlugin("com.DrBibop.VRMod", "VR Mod", "1.1.0")]
+    public class VRMod : BaseUnityPlugin
     {
         private void Awake()
         {
             On.RoR2.UI.HUD.Awake += AdjustHUDAnchors;
             On.RoR2.CameraRigController.GetCrosshairRaycastRay += GetVRCrosshairRaycastRay;
+            On.RoR2.MatchCamera.Awake += (orig, self) =>
+            {
+                self.matchFOV = false;
+                orig(self);
+            };
+            IL.RoR2.CameraRigController.SetCameraState += (il) =>
+            {
+                ILCursor c = new ILCursor(il);
+                c.GotoNext(
+                    x => x.MatchLdarg(0),
+                    x => x.MatchLdarg(1),
+                    x => x.MatchLdfld<CameraState>("fov")
+                    );
+                
+                ILLabel breakLabel = c.IncomingLabels.ToList().First<ILLabel>();
 
-            //On.RoR2.UI.MainMenu.MainMenuController.Start += EnableVR;
+                while(c.Next.OpCode.Code != Mono.Cecil.Cil.Code.Ret)
+                {
+                    c.Remove();
+                }
+
+                ILLabel retLabel = c.MarkLabel();
+
+                c.GotoPrev(
+                    x => x.MatchBr(breakLabel)
+                    );
+                c.Remove();
+                c.Emit(Mono.Cecil.Cil.OpCodes.Br_S, retLabel);
+            };
         }
 
         private void AdjustHUDAnchors(On.RoR2.UI.HUD.orig_Awake orig, RoR2.UI.HUD self)
@@ -61,56 +91,6 @@ namespace DrBibop
             }
         }
 
-        /*
-        private void EnableVR(On.RoR2.UI.MainMenu.MainMenuController.orig_Start orig, RoR2.UI.MainMenu.MainMenuController self)
-        {
-            orig(self);
-
-            //Credit to x753 for the bytes operations
-
-            string[] oldHexStrings = new string[]
-            {
-                "0000049000588F8800000011000010000000000032303138",
-                "726C642E756E6974790000000000000000000000",
-                "9C1401000A0000000C0000000000000050600200940000000B0000000D00000000000000E8600200080D56000C0000000E00000000000000F06D5800840100000D0000000F00000000000000786F5800140100000E000000110000000000000090705800EC0D00000F0000001200000000000000807E5800D4000000100000001300000000000000587F58000000000011000000140000000000000058"
-            };
-
-            string[] newHexStrings = new string[]
-            {
-                "0000049000588FA800000011000010000000000032303138",
-                "726C642E756E6974790000000000000003000000060000004F63756C75730000060000004F70656E56520000040000004E6F6E65",
-                "BC1401000A0000000C0000000000000070600200940000000B0000000D0000000000000008610200080D56000C0000000E00000000000000106E5800840100000D0000000F00000000000000986F5800140100000E0000001100000000000000B0705800EC0D00000F0000001200000000000000A07E5800D4000000100000001300000000000000787F58000000000011000000140000000000000078"
-            };
-
-            byte[] allBytes = File.ReadAllBytes(Environment.CurrentDirectory + @"\Risk of Rain 2_Data\globalgamemanagers");
-
-            for (int i = 0; i < 3; i++)
-            {
-                byte[] oldBytePattern = BytePatternUtilities.ConvertHexStringToByteArray(oldHexStrings[i]);
-
-                byte[] newBytePattern = BytePatternUtilities.ConvertHexStringToByteArray(newHexStrings[i]);
-
-                allBytes = BytePatternUtilities.ReplaceBytes(allBytes, oldBytePattern, newBytePattern);
-
-                if (allBytes == null)
-                    break;
-            }
-
-            if (allBytes != null)
-            {
-                File.WriteAllBytes(Environment.CurrentDirectory + @"\Risk of Rain 2_Data\globalgamemanagers", resultBytes);
-                GameObject restartWarning = new GameObject("RestartWarning");
-                restartWarning.AddComponent<RestartWarning>();
-                DontDestroyOnLoad(restartWarning);
-            }
-            else
-            {
-                var harmony = new Harmony("com.DrBibop.VRFixes");
-                harmony.PatchAll();
-            }
-        }
-        */
-
         private Ray GetVRCrosshairRaycastRay(On.RoR2.CameraRigController.orig_GetCrosshairRaycastRay orig, RoR2.CameraRigController self, Vector2 crosshairOffset, Vector3 raycastStartPlanePoint)
         {
             if (!self.sceneCam)
@@ -124,78 +104,4 @@ namespace DrBibop
             return new Ray(Vector3.ProjectOnPlane(self.sceneCam.transform.position - raycastStartPlanePoint, self.sceneCam.transform.rotation * Vector3.forward) + raycastStartPlanePoint, quaternion * Vector3.forward);
         }
     }
-
-    /*
-    public class RestartWarning : MonoBehaviour
-    {
-        void OnGUI()
-        {
-            GUI.Label(new Rect(0f, 0f, 600f, 80f), "The Unity Audio Engine has been restored, please restart the game once for it to take effect.", GUI.skin.label);
-        }
-    }
-    
-
-    //By x753
-    public static class BytePatternUtilities
-    {
-        public static byte[] ConvertHexStringToByteArray(string hexString)
-        {
-            byte[] data = new byte[hexString.Length / 2];
-            for (int i = 0; i < data.Length; i++)
-            {
-                string byteValue = hexString.Substring(i * 2, 2);
-                data[i] = byte.Parse(byteValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
-            }
-
-            return data;
-        }
-
-        private static int FindBytes(byte[] src, byte[] find)
-        {
-            int index = -1;
-            int matchIndex = 0;
-            for (int i = 0; i < src.Length; i++)
-            {
-                if (src[i] == find[matchIndex])
-                {
-                    if (matchIndex == (find.Length - 1))
-                    {
-                        index = i - matchIndex;
-                        break;
-                    }
-                    matchIndex++;
-                }
-                else
-                {
-                    matchIndex = 0;
-                }
-
-            }
-            return index;
-        }
-
-        public static byte[] ReplaceBytes(byte[] src, byte[] search, byte[] repl)
-        {
-            byte[] dst = null;
-            byte[] temp = null;
-            int index = FindBytes(src, search);
-            while (index >= 0)
-            {
-                if (temp == null)
-                    temp = src;
-                else
-                    temp = dst;
-
-                dst = new byte[temp.Length - search.Length + repl.Length];
-
-                Buffer.BlockCopy(temp, 0, dst, 0, index);
-                Buffer.BlockCopy(repl, 0, dst, index, repl.Length);
-                Buffer.BlockCopy(temp, index + search.Length, dst, index + repl.Length, temp.Length - (index + search.Length));
-
-                index = FindBytes(dst, search);
-            }
-            return dst;
-        }
-    }
-    */
 }
---
+++ VREnabler/VREnabler.cs
@@ -0,0 +1,159 @@
+﻿using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+using BepInEx.Logging;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace QModManager
+{
+    //Class by MrPurple
+
+    /// <summary>
+    /// A patcher which runs ahead of UnityPlayer to enable VR in the Global Game Manager.
+    /// </summary>
+    public static class VREnabler
+    {
+        internal static string VREnablerPath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        internal static string GameRootPath => BepInEx.Paths.GameRootPath;
+        internal static string ManagedPath => BepInEx.Paths.ManagedPath;
+
+        private static readonly ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource("VREnabler");
+
+        /// <summary>
+        /// Called from BepInEx while patching, our entry point for patching.
+        /// Do not change the method name as it is identified by BepInEx. Method must remain public.
+        /// </summary>
+        [Obsolete("Should not be used!", true)]
+        public static void Initialize()
+        {
+            EnableVROptions(Path.Combine(ManagedPath, "../globalgamemanagers"), Path.Combine(ManagedPath, "../Plugins"));
+        }
+
+        private static void EnableVROptions(string assetsPath, string pluginsPath)
+        {
+            Logger.LogInfo("Enabling VR...");
+
+            AssetsManager am = new AssetsManager();
+            AssetsFileInstance afi = am.LoadAssetsFile(assetsPath, false);
+            am.LoadClassDatabase(Path.Combine(VREnablerPath, "cldb.dat"));
+
+
+            for (int i = 0; i < afi.table.assetFileInfoCount; i++)
+            {
+                try
+                {
+                    AssetFileInfoEx info = afi.table.GetAssetInfo(i);
+                    AssetTypeInstance ati = am.GetATI(afi.file, info);
+                    AssetTypeValueField baseField = ati?.GetBaseField();
+
+                    AssetTypeValueField enabledVRDevicesField = baseField?.Get("enabledVRDevices");
+
+                    if (enabledVRDevicesField is null)
+                        continue;
+
+                    AssetTypeValueField vrArrayField = enabledVRDevicesField.Get("Array");
+
+                    if (vrArrayField is null)
+                        continue;
+
+
+                    AssetTypeValueField Oculus = ValueBuilder.DefaultValueFieldFromArrayTemplate(vrArrayField);
+                    Oculus.GetValue().Set("Oculus");
+                    AssetTypeValueField OpenVR = ValueBuilder.DefaultValueFieldFromArrayTemplate(vrArrayField);
+                    OpenVR.GetValue().Set("OpenVR");
+                    AssetTypeValueField None = ValueBuilder.DefaultValueFieldFromArrayTemplate(vrArrayField);
+                    None.GetValue().Set("None");
+
+                    vrArrayField.SetChildrenList(new AssetTypeValueField[] { Oculus, OpenVR, None });
+
+                    byte[] vrAsset;
+                    using (MemoryStream memStream = new MemoryStream())
+                    using (AssetsFileWriter writer = new AssetsFileWriter(memStream))
+                    {
+                        writer.bigEndian = false;
+                        baseField.Write(writer);
+                        vrAsset = memStream.ToArray();
+                    }
+
+                    List<AssetsReplacer> rep = new List<AssetsReplacer>() { new AssetsReplacerFromMemory(0, i, (int)info.curFileType, 0xFFFF, vrAsset) };
+
+                    using (MemoryStream memStream = new MemoryStream())
+                    using (AssetsFileWriter writer = new AssetsFileWriter(memStream))
+                    {
+                        afi.file.Write(writer, 0, rep, 0);
+                        afi.stream.Close();
+                        File.WriteAllBytes(assetsPath, memStream.ToArray());
+                    }
+
+                    Logger.LogInfo("Successfully enabled VR!");
+
+                    Logger.LogInfo("Checking for VR plugins...");
+
+                    DirectoryInfo gamePluginsDirectory = new DirectoryInfo(pluginsPath);
+
+                    string[] pluginNames = new string[]
+                    {
+                        "AudioPluginOculusSpatializer.dll",
+                        "openvr_api.dll",
+                        "OVRGamepad.dll",
+                        "OVRPlugin.dll"
+                    };
+
+                    FileInfo[] gamePluginFiles = gamePluginsDirectory.GetFiles();
+
+                    bool hasCopied = false;
+
+                    foreach (string pluginName in pluginNames)
+                    {
+                        if (!Array.Exists<FileInfo>(gamePluginFiles, (file) => pluginName == file.Name))
+                        {
+                            hasCopied = true;
+                            using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("VREnabler.VRPlugins." + pluginName))
+                            {
+                                using (var file = new FileStream(Path.Combine(gamePluginsDirectory.FullName, pluginName), FileMode.Create, FileAccess.Write))
+                                {
+                                    Logger.LogInfo("Copying " + pluginName);
+                                    resource.CopyTo(file);
+                                }
+                            }
+                        }
+                    }
+
+                    if (hasCopied)
+                        Logger.LogInfo("Successfully copied VR plugins!");
+                    else
+                        Logger.LogInfo("VR plugins already present");
+
+                    return;
+                }
+                catch { }
+            }
+
+            Logger.LogError("VR enable location not found!");
+
+        }
+
+        /// <summary>
+        /// For BepInEx to identify your patcher as a patcher, it must match the patcher contract as outlined in the BepInEx docs:
+        /// https://bepinex.github.io/bepinex_docs/v5.0/articles/dev_guide/preloader_patchers.html#patcher-contract
+        /// It must contain a list of managed assemblies to patch as a public static <see cref="IEnumerable{T}"/> property named TargetDLLs
+        /// </summary>
+        [Obsolete("Should not be used!", true)]
+        public static IEnumerable<string> TargetDLLs { get; } = new string[0];
+
+        /// <summary>
+        /// For BepInEx to identify your patcher as a patcher, it must match the patcher contract as outlined in the BepInEx docs:
+        /// https://bepinex.github.io/bepinex_docs/v5.0/articles/dev_guide/preloader_patchers.html#patcher-contract
+        /// It must contain a public static void method named Patch which receives an <see cref="AssemblyDefinition"/> argument,
+        /// which patches each of the target assemblies in the TargetDLLs list.
+        /// 
+        /// We don't actually need to patch any of the managed assemblies, so we are providing an empty method here.
+        /// </summary>
+        /// <param name="ad"></param>
+        [Obsolete("Should not be used!", true)]
+        public static void Patch(AssemblyDefinition ad) { }
+    }
+}
